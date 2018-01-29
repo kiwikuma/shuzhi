72f48cab77ce6f95258dbfc593479e8e54dd2d31
kiwikuma <anfangwf@outlook.com>
2018-01-29 22:56:25 +0100

    12

--- new8/evaluate_stat.m
+++ new8/evaluate_stat.m
@@ -1,23 +1,24 @@
 function [elemat,elevec] = evaluate_stat(elenodes,gpx,gpw)
-gpx = gx2dref(3);
-gpw = gw2dref(3);
-lamda = 48;
-elemat = zeros(4);
-elevec = zeros(4,1);
-q_punkt = 0;
 
-for i = 1:4
-    for k = 1:size(gpx,1)
-        dN = linquadderivref_N(gpx(k,1),gpx(k,2));
-        N = linquadref_N(gpx(k,1),gpx(k,2));
-        [detJ,invJ] = getdetJacob(elenodes,gpx(k,1),gpx(k,2));
-        for j = 1:4
-            elemat(i,j) = elemat(i,j)+lamda*(invJ*dN(:,i))'*(invJ*dN(:,j))*detJ*gpw(k);
-        end
-        elevec(i) = elevec(i) + q_punkt*N(i)*detJ*gpw(k);
-    end
-end
-end
-            
-              
+deriv_N = [];
+J = [];
+detJ = [];
+InvJ = [];
+lambda = 48;
 
+A = zeros(4,4);
+
+
+for k = 1:length(gpx)
+deriv_N = linquadderivref(gpx(k,1), gpx(k,2));
+[J,detJ,InvJ] = getJacobian(elenodes, gpx(k,1), gpx(k,2));
+
+ for i = 1:4
+     for j = 1:4
+         A(i,j) = A(i,j) + lambda*(deriv_N(i,:)*InvJ)*(deriv_N(j,:)*InvJ)' *detJ*gpw(k,1);
+     end
+ end
+end 
+
+elemat = A;
+ 
